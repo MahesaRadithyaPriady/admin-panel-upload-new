4927b364c028882e311eb01c948cc7d3ad0f91e7
MahesaRadithyaPriady <Mahesaradithyapriady@gmail.com>
2025-12-20 05:07:49 +0700

    add script migrate

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-   
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // izinkan semua host
+    port: 5173,
+    allowedHosts: ['localhost', 'upload.nanimeid.xyz'], // whitelist host dev
+  },
+  preview: {
+    port: 4173,
+    host: true,
+    allowedHosts: ['upload.nanimeid.xyz'], // whitelist host preview
+  },
 })
