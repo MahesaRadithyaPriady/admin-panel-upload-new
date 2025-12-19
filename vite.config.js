f565fc50b9b4fe99bee8fdb8b1c3d5845bfd5dd7
MahesaRadithyaPriady <Mahesaradithyapriady@gmail.com>
2025-12-20 05:04:42 +0700

    add script migrate

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
-import fs from 'fs'
-import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'cert/localhost-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'cert/localhost-cert.pem')),
-    },
-    host: 'localhost',
-    port: 5173,
-  },
+  plugins: [react(),tailwindcss()],
+   
 })
